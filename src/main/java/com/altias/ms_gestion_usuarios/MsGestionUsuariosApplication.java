225c5285d4e8f876f01b1d9f4ff98046eaf982d8
egon von <eg.vonfurstenberg@duocuc.cl>
2025-06-05 22:00:13 -0400

    paquete inicial

---
+++ src/main/java/com/altias/ms_gestion_usuarios/MsGestionUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.altias.ms_gestion_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionUsuariosApplication.class, args);
+	}
+
+}
